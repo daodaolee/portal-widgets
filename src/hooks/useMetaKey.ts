f9d09bd51fed3637de7db8c2c8e3a4e1bbeea591
daodaolee <im@daodaolee.cn>
2022-08-25 01:03:29 +0800

    fix: 修改win快捷键错误问题

--- src/hooks/useMetaKey.ts
+++ src/hooks/useMetaKey.ts
@@ -1,3 +1,23 @@
+/**
+ * 判断系统类型
+ * @returns 系统型号 win / mac
+ */
+function systemInfo() {
+  var agent = navigator.userAgent.toLowerCase()
+  var isMac = /macintosh|mac os x/i.test(navigator.userAgent)
+  if (agent.indexOf('win') >= 0 || agent.indexOf('wow32') >= 0) {
+    return 'win'
+  }
+  if (isMac) {
+    return 'mac'
+  }
+}
+
 export default function (e: KeyboardEvent) {
-  return e.metaKey ? '_blank' : '_self'
+  const info = systemInfo()
+  if (info === 'win') {
+    return e.ctrlKey ? '_blank' : '_self'
+  } else if (info === 'mac') {
+    return e.metaKey ? '_blank' : '_self'
+  }
 }
